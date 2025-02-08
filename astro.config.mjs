095865819ff3f34289b860ef0b7d00dc3baf007a
Perez2121pv <uriperez4142@gmail.com>
2025-02-07 20:40:55 -0600

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sistemap";
 import tailwind from '@astrojs/tailwind';
 
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = 'https://localhost:${SERVER_PORT}';
+
+const LIVE_URL = "https://uriel-P-V.github.io";
+
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+
+if(isBuild){
+  BASE_URL =LIVE_URL;
+}
+
 export default defineConfig({
-  site: 'https://uriel-P-V.github.io/mi-portfolio', // Asegúrate de poner la URL correcta de tu repositorio
-  base: '/mi-portfolio'
+  server: {port: SERVER_PORT},
+  base: '/mi-portfolio',
   integrations: [
+    sitemap(),
     tailwind() // Integración de TailwindCSS
   ],
 });
